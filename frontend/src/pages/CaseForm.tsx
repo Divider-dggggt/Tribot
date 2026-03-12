93c7e53fcc0b44afe477b98d74517ae65583ee74
Tony Nguyen <107409082+proxyTony@users.noreply.github.com>
2026-03-12 22:33:11 +1100

    Add initial form for new triage cases

--- frontend/src/pages/CaseForm.tsx
+++ frontend/src/pages/CaseForm.tsx
@@ -1,7 +1,34 @@
 import React, { ReactElement } from "react";
+import { useForm } from "react-hook-form";
+import { TextField, Button } from '@mui/material';
 
 export const CaseForm = (): ReactElement => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = (data: Record<string, string>) => console.log(data);
+
   return (
-    <>TODO: case form</>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        label="Patient ID"
+        {...register("patientID", { required: "Required" })}
+        error={!!errors.patientID}
+        helperText={errors.patientID?.message as string}
+      />
+      <TextField
+        label="Patient Name"
+        {...register("patientName", { required: "Required" })}
+        error={!!errors.patientName}
+        helperText={errors.patientName?.message as string}
+      />
+      <TextField
+        label="Symptom Description"
+        {...register("symptoms", { required: "Required" })}
+        multiline
+        rows={10}
+        error={!!errors.symptoms}
+        helperText={errors.symptoms?.message as string}
+      />
+      <Button type="submit" variant="contained">Submit for Triage</Button>
+    </form>
   );
 };
